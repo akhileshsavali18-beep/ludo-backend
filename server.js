1fa67622caba9d1fb5636f3aa9d9c6de04d313ad
akhileshsavali18-beep <akhileshsavali18@gmail.com>
2026-03-30 09:27:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app); // 🔴 THIS LINE MUST EXIST
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
