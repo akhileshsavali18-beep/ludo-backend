50a0debb7afe8456d427bc2f7b59acf9f1ce4bba
akhileshsavali18-beep <akhileshsavali18@gmail.com>
2026-03-30 09:33:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,3 +18,6 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log("Server running on port", PORT);
 });
+app.get("/", (req, res) => {
+  res.send("Ludo Backend Running");
+});
