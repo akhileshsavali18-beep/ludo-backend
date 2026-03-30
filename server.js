b314e3ab65256fb3b056f70c066b7ccbd1a9cb73
akhileshsavali18-beep <akhileshsavali18@gmail.com>
2026-03-30 09:59:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,3 +21,36 @@ server.listen(PORT, () => {
 app.get("/", (req, res) => {
   res.send("Ludo Backend Running");
 });
+socket.on("createRoom", ({ username }) => {
+  const roomCode = generateRoomCode();
+
+  rooms[roomCode] = {
+    host: socket.id,
+    players: [{ id: socket.id, username }],
+  };
+
+  socket.join(roomCode);
+
+  // 🔴 THIS IS CRITICAL
+  socket.emit("roomCreated", {
+    roomCode,
+    players: rooms[roomCode].players,
+  });
+});
+socket.on("joinRoom", ({ roomCode, username }) => {
+  const room = rooms[roomCode];
+
+  if (!room) {
+    return socket.emit("error", "Room not found");
+  }
+
+  if (room.players.length >= 4) {
+    return socket.emit("error", "Room full");
+  }
+
+  room.players.push({ id: socket.id, username });
+  socket.join(roomCode);
+
+  // 🔴 THIS IS CRITICAL
+  io.to(roomCode).emit("playerJoined", room.players);
+});
